721ea2e7f7efd98b0510220666dcf795cf5cffa6
miku <mikumarkus@gmail.com>
2026-03-31 20:20:55 +0300

    Application performs required tasks and supports Finnish, Swedish and English. Tests not yet done

---
+++ src/main/java/ShoppingCart.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<Item> cart = new ArrayList<>();
+    private double totalValue = 0;
+
+    public void addItem(Item i){
+        cart.add(i);
+        totalValue += i.getValue()*i.getQuantity();
+    }
+
+    public void removeItem(Item i){
+        cart.remove(i);
+        totalValue -= i.getValue()*i.getQuantity();
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+}
